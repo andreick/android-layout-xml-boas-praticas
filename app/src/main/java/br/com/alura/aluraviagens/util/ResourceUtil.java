24a37e923eaebe548d375d4fd63630129b6bff3d
Andreick <andreick.gomes.ismart@gmail.com>
2022-12-11 16:27:58 -0300

    atualização do projeto 2022

--- app/src/main/java/br/com/alura/aluraviagens/util/ResourceUtil.java
+++ app/src/main/java/br/com/alura/aluraviagens/util/ResourceUtil.java
@@ -4,14 +4,15 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 
+import androidx.core.content.ContextCompat;
+
 public class ResourceUtil {
 
     public static final String DRAWABLE = "drawable";
 
     public static Drawable devolveDrawable(Context context, String drawableEmTexto) {
-        Resources resources = context.getResources();
-        int idDoDrawable = resources.getIdentifier(drawableEmTexto
-                , DRAWABLE, context.getPackageName());
-        return resources.getDrawable(idDoDrawable);
+        int idDoDrawable = context.getResources()
+                .getIdentifier(drawableEmTexto, DRAWABLE, context.getPackageName());
+        return ContextCompat.getDrawable(context, idDoDrawable);
     }
 }
